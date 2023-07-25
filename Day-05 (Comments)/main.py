b0ebe6c6684e184691b37071835089b7e00738ba
Hafsa Imtiaz <136093032+CodeWithHafsa@users.noreply.github.com>
2023-07-25 10:36:34 -0700

    Comments, Escape Sequence and Print Statement

---
+++ Day-05 (Comments)/main.py
@@ -0,0 +1,17 @@
+#COMMENTS AND PRINT STATEMENT:
+
+# Hey, Do not remove this line.
+# Author: Hafsa
+# Course: 100DaysofCode
+print("This is a print statement \nThis is a new line")
+print("Hello World!!!") #Printing Hello World
+
+# Back-Slash Double quote:
+print("This is a \"print statement\" and this is a new line")
+# Back-Slash Single quote:
+print('This is\' a \"print statement\" and this is a new line')
+
+#Print statement
+# print("Hey", 6, 7, sep"~" )
+# print("Hey", 6, 7, sep"~", end="009\n")
+# print("Hafsa")
